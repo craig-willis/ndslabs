dffc984e28ec6a23f0e5e337cbeb703cae1d5537
Craig Willis <willis8@illinois.edu>
2018-03-14 15:13:02 -0500

    Temporary fix for glide build issues

--- apiserver/pkg/kube/kube.go
+++ apiserver/pkg/kube/kube.go
@@ -1153,7 +1153,14 @@ func (k *KubeHelper) Exec(pid string, pod string, container string, kube *KubeHe
 			}
 		}()
 
-		err = e.Stream(remotecommandserver.SupportedStreamingProtocols, inr, outw, nil, true)
+		err = e.Stream(remotecommand.StreamOptions{
+			SupportedProtocols: remotecommandserver.SupportedStreamingProtocols,
+			Stdin:              inr,
+			Stdout:             outw,
+			Tty:                true,
+		})
+
+		//err = e.Stream(remotecommandserver.SupportedStreamingProtocols, inr, outw, nil, true)
 		if err != nil {
 			glog.Error(err)
 			return
@@ -1558,7 +1565,15 @@ func (k *KubeHelper) ExecCommand(pid string, pod string, command []string) (stri
 
 	localOut := &bytes.Buffer{}
 	localErr := &bytes.Buffer{}
-	err = e.Stream(remotecommandserver.SupportedStreamingProtocols, nil, localOut, localErr, false)
+	//err = e.Stream(remotecommandserver.SupportedStreamingProtocols, nil, localOut, localErr, false)
+
+	err = e.Stream(remotecommand.StreamOptions{
+		SupportedProtocols: remotecommandserver.SupportedStreamingProtocols,
+		Stdin:              nil,
+		Stdout:             localOut,
+		Stderr:             localErr,
+		Tty:                true,
+	})
 	return localOut.String(), err
 }
 
